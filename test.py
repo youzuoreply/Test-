99b84a36e1153b3f6cd67a2bbc880256ad01f14a
youzuoreply <y.zuo@reply.de>
2026-01-27 15:26:19 +0100

    some change

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ model = YOLO('yolov8n.pt')  # nano model (fastest)
 # Run inference on an image
 results = model.predict(source='testimage.jpg')
 
-# Display results
 for result in results:
     print(result)
     result.save(filename='output.jpg')
