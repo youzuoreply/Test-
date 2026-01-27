9d4613f7c4b98b019e4d0d7efada239542abba31
youzuoreply <y.zuo@reply.de>
2026-01-27 15:57:31 +0100

    yolo

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Load a pretrained YOLOv8 model
 model = YOLO('yolov8n.pt')  # nano model (fastest)
-
+#yolo
 # Run inference on an image
 results = model.predict(source='testimage.jpg')
 
