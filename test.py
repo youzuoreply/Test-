cf4aed31eed20b3db8deefb1d29de02699851b1f
youzuoreply <y.zuo@reply.de>
2026-01-28 10:55:56 +0100

    yoo

--- test.py
+++ test.py
@@ -3,14 +3,3 @@ from ultralytics import YOLO
 print("i changed")
 model = YOLO('yolov8n.pt')  # nano model (fastest)
 
-
-results = model.predict(source='testimage.jpg')
-
-for result in results:
-    print(result)
-    result.save(filename='output.jpg')
-
-print("Inference complete. Results saved to output.jpg")
-
-if True:
-    print("This block always executes.")
